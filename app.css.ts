c27c89713d261289715e2ac2c77ce67d1bfeaa54
kavya12 <kavya1292000@gmail.com>
2023-12-27 10:36:45 +0000

    done

---
+++ app.css.ts
@@ -0,0 +1,63 @@
+* {
+
+  box-sizing: border-box;
+
+  -webkit-box-sizing: border-box;
+
+  -moz-box-sizing: border-box;
+
+  font: 300 15px/1.4 -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+
+}
+
+
+
+html, body {
+
+  height: 100%;
+
+  margin: 0;
+
+  padding: 0;
+
+  background: #fff;
+
+  -webkit-font-smoothing: antialiased;
+
+  color: #545e6f;
+
+}
+
+
+
+img {
+
+  vertical-align: bottom;
+
+}
+
+img a {
+
+  border: 0;
+
+}
+
+ul,
+
+ol {
+
+  list-style: none;
+
+  margin: 0;
+
+  padding: 0;
+
+}
+
+a {
+
+  color: #9f72e6;
+
+  text-decoration: none;
+
+}
